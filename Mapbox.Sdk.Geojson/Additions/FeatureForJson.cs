061b82939d5b7415c65b622334900117e4d44873
BradKwon <brad.kwon@mindtags.de>
2018-07-25 14:59:43 +0200

    Added FeatureForJson class to support Feature class.
    Due to being unable to use FromGeometry method.

---
+++ Mapbox.Sdk.Geojson/Additions/FeatureForJson.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Com.Mapbox.Geojson.Additions
+{
+    public class FeatureForJson
+    {
+        [JsonProperty("type")]
+        public string Type { get { return "Feature"; } }
+
+        [JsonProperty("properties")]
+        public Properties Properties { get; set; }
+
+        [JsonProperty("geometry")]
+        public DatasetGeometry Geometry { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+
+    public class Properties
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("mt-name")]
+        public string MtName { get; set; }
+    }
+
+    public class DatasetGeometry
+    {
+        [JsonProperty("coordinates")]
+        public IList<object> Coordinates { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+}
